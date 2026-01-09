1c6c898e5718aa12af0f35fb50c01c577203e4d8
fly6516 <fly6516@outlook.com>
2026-01-09 13:59:00 +0800

    feat(datagenerator): 补足数据生成器的示例文件

---
+++ src/client/java/com/simpletombstone/SimpletombstoneDataGenerator.java
@@ -0,0 +1,12 @@
+package com.simpletombstone;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.jetbrains.annotations.NotNull;
+
+public class SimpletombstoneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(@NotNull FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
